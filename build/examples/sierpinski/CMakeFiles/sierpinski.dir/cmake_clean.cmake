29beb3d0af0a0e28d9a67bb7bee10701f21c964b
vbonelli <vc.cb00@gmail.com>
2021-03-14 22:44:23 -0300

    primeiro commit

---
+++ build/examples/sierpinski/CMakeFiles/sierpinski.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../../bin/sierpinski"
+  "../../bin/sierpinski.pdb"
+  "CMakeFiles/sierpinski.dir/main.cpp.o"
+  "CMakeFiles/sierpinski.dir/openglwindow.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sierpinski.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/examples/tictactoe/CMakeFiles/tictactoe.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../../bin/tictactoe"
+  "../../bin/tictactoe.pdb"
+  "CMakeFiles/tictactoe.dir/main.cpp.o"
+  "CMakeFiles/tictactoe.dir/openglwindow.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tictactoe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
